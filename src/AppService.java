5b82bccc7bae782b47b17ba65a0de78038cdfac3
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-06-23 22:06:14 -0400

    Implement loop for inputting calc

--- src/AppService.java
+++ src/AppService.java
@@ -1,6 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppService {
+    final private List<String> msgs;
+    private String x;
+    private String operator;
+    private String y;
 
     public AppService() {
+        this.msgs = new ArrayList<String>();
+        this.msgs.add("Enter an equation");
+        this.msgs.add("Do you even know what numbers are? Stay focused!");
+        this.msgs.add("Yes ... an interesting math operation. You've slept through all classes, haven't you?");
+        this.x = this.operator = this.y = "";
+    }
 
+    public String getMsg(int index) {
+        if (index >= this.msgs.size()) {
+            return "No message found";
+        }
+        return this.msgs.get(index);
+    }
+
+    public void setCalc(String x, String operator, String y) {
+        this.x = x;
+        this.operator = operator;
+        this.y = y;
+    }
+
+    public boolean isNumericRegex(String str) {
+        return str != null && str.matches("-?\\d+(\\.\\d+)?");
+    }
+
+    public boolean isOperator(String str) {
+        return str != null && (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/"));
     }
 }
--- src/TextUI.java
+++ src/TextUI.java
@@ -10,7 +10,31 @@ public class TextUI {
     }
 
     public void start() {
+        while (true) {
+            this.printMsg(0);
+            if (this.receiveCalcInput()) {
+                break;
+            }
+        }
+    }
 
+    private void printMsg(int index) {
+        System.out.println(this.service.getMsg(index));
+    }
+
+    private boolean receiveCalcInput() {
+        String input = this.scanner.nextLine();
+        String[] words = input.split("\\s+");
+
+        if (!this.service.isNumericRegex(words[0]) || !this.service.isNumericRegex(words[2])) {
+            this.printMsg(1);
+            return false;
+        } else if (!this.service.isOperator(words[1])) {
+            this.printMsg(2);
+            return false;
+        }
+        this.service.setCalc(words[0], words[1], words[2]);
+        return true;
     }
 
 }
